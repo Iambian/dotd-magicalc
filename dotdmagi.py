982133b4dbb537961905362345ac3772b26c8f4b
rawrs <iambian@gmail.com>
2019-08-02 21:51:02 -0400

    Initial commit

---
+++ dotdmagi.py
@@ -0,0 +1,651 @@
+''' Best Magic Calculator for Dawn of the Dragons
+    usage: dotdmagi.py numsspellslots tag1 [tag2 [tag3...]]
+    returns: Details of best magics from best to worst and a short string to
+             paste on raids regarding which magics should be used
+'''
+
+import sys,os,copy,math
+try:
+    SLOTNUM = int(sys.argv[1])
+    EXTRAFUNC = ''
+except:
+    SLOTNUM = 0
+    EXTRAFUNC = sys.argv[1]
+SHOWDEBUG = False
+USERAREMAGIC = False
+RAIDTAGS = sys.argv[2:]
+MAGICLIST = []
+
+OWNED = {
+    'MOUNTS':400,
+    'LEGIONS':100,
+    'MAGICS':30,
+    'SPELLS':['TotH',"IS","FS"],
+    'GIANTESSENCE':1,
+    'BEASTMANESSENCE':18,
+    'FESTIVALESSENCE':6,
+    'UNDERGROUNDESSENCE':15,
+    'DWARFTROOPS':10,
+    'DWARFGENERALS':5,
+    'MAGICALBEINGTROOPS':10,
+    'MAGICALBEINGGENERALS':10,
+    'DRAGONESSENCE':10,
+    'DRAGONMOUNTS':20,
+    
+}
+if EXTRAFUNC == 'showparams':
+    print "Showing internal parameters of magic calc, dict: OWNED"
+    for i in OWNED:
+        print str(i)+": "+str(OWNED[i])
+    sys.exit()
+
+
+''' Class documentation: Magic()
+    Uses a reverse-creation scheme.
+    0.  Instantiate a Magic() class with fullname and nickname.
+    1.  Define triggers for a proc. All triggers per damage proc must match
+        before damage for that proc is processed
+    2.  Define damage for previously defined triggers. Doing this flushes the
+        trigger table and primes class to accept new triggers for a new damage.
+        This may be a lambda function in case damage is based on trigger params
+        Unconditional damage is done without defining new triggers.
+    3.  Define proc chance. This ties in all previously defined damage triggers
+        and flushes the damage and trigger tables. At this point, most magics
+        are complete in definition.
+    Supported triggers:
+    'raidtag':  Matches against supplied input tags. More than one tag per
+                trigger indicates an OR situation where any one tag may match.
+    'spellcast':    Matches against whether or not a spell is previously cast.
+                    This allows an optimiation via iteration of magic assembly.
+    'spellowned':   Matches against spells in the ownership static list
+
+'''
+
+
+
+class Magic(object):
+    spelllist = []
+    magic_id = 1
+    def __init__(self,fullname,nickname):
+        self.proclist = []  #list of 2-lists of [procrate,[listofprocs]]
+        self.curproc = list()   #proc entry [procDamage, {triggercond:triggerdata,...}]
+        self.temptrigger = dict()
+        self.fullname = fullname
+        self.nickname = nickname
+        self.israre = False
+        
+        self.id = Magic.magic_id
+        Magic.magic_id += 1
+        Magic.spelllist.append(self)
+        
+    def newTrig(self,triggertype,triggerdata):
+        if not isinstance(triggerdata,(list,tuple)):
+            triggerdata = [triggerdata]
+        self.temptrigger[triggertype] = triggerdata
+    def newTrigTag(self,triggerdata):
+        self.newTrig('raidtag',triggerdata)
+    def newDmg(self,procdamage):
+        self.curproc.append([procdamage,copy.deepcopy(self.temptrigger)])
+        self.temptrigger = dict()
+    def newProc(self,procrate):
+        self.proclist.append([procrate,copy.deepcopy(self.curproc)])
+        self.curproc = list()
+    def setRare(self):
+        self.israre = True
+    def setrare(self): self.setRare() #convenience
+        
+    @classmethod
+    def getSpell(cls,spellid):
+        for i in cls.spelllist:
+            if i.id == spellid:
+                return i
+        return cls.spelllist[0]
+        
+    @staticmethod
+    def getID(name_or_nick):
+        for spell in Magic.spelllist:
+            if name_or_nick == spell.fullname or name_or_nick == spell.nickname:
+                return spell.id
+        return 0
+        
+    def getAvg(self):
+        global RAIDTAGS,MAGICLIST,OWNED,USERAREMAGIC,SHOWDEBUG
+        curproctotal = 0
+        if self.israre and not USERAREMAGIC:
+            return 0
+        #print "Proclist: "+str(self.proclist)
+        for procs in self.proclist:
+            procrate = procs[0]
+            if SHOWDEBUG: print "procrate: "+str(procrate)+", numprocs in this rate: "+str(len(procs[1]))
+            for proc in procs[1]:
+                procdamage = proc[0]
+                triggered = 0
+                triggers = proc[1]
+                if SHOWDEBUG: print "procdamage: "+str(procdamage)
+                if SHOWDEBUG: print "triggers: "+str(triggers)
+                if 'none' in triggers:
+                    triggered += 1
+                if 'raidtag' in triggers:
+                    for tag in RAIDTAGS:
+                        if tag in triggers['raidtag']:
+                            triggered += 1
+                            break
+                if 'spellcast' in triggers:
+                    for magic in MAGICLIST:
+                        magic = magic[0]  #First entry is actual spell
+                        data = triggers['spellcast']
+                        #convert spell names into IDs to membership test against
+                        for i in range(len(data)):
+                            if isinstance(data[i],str):
+                                data[i] = self.getID(data[i])
+                        if magic.id in data:
+                            triggered += 1
+                            break
+                if 'spellowned' in triggers:
+                    for magic in OWNED['SPELLS']:
+                        id = self.getID(magic)
+                        data = triggers['spellowned']
+                        #convert spell names into IDs to membership test against
+                        for i in range(len(data)):
+                            if isinstance(data[i],str):
+                                data[i] = self.getID(data[i])
+                        if id in data:
+                            triggered += 1
+                            break
+                #
+                #add in other trigger conditions
+                #
+                if triggered == len(triggers):
+                    if SHOWDEBUG: print "Condition true"
+                    if callable(procdamage):
+                        procdamage = procdamage()
+                    curproctotal += round((procdamage * procrate) / 100.0,2)
+                else:
+                    if SHOWDEBUG: print "Condition false"
+        return curproctotal
+## ----------------------------- MAGIC DATA ENTRY --------------------------- ##
+Magic("NULL_ENTRY","NULL_ENTRY")
+#
+m = Magic("Traditions of the Hunt","TotH")
+m.newTrigTag('beast')
+m.newDmg(300)
+m.newTrig('spellowned','TotH')
+m.newTrigTag('beast')
+m.newDmg(250)
+m.newDmg(250)  #unconditional
+m.newProc(15)
+#
+m = Magic("Mark of the Infinite Dawn","MID")
+#   This magic define does not account for infinite dawn set ownership.
+#   Any increase in this case will be negligible.
+m.newDmg(1980)
+m.newTrigTag('dragon')
+m.newDmg(590)
+m.newTrigTag('worldraid')
+m.newDmg(590)
+m.newTrigTag('eventraid')
+m.newDmg(590)
+m.newTrigTag('elite')
+m.newDmg(590)
+m.newTrigTag('deadly')
+m.newDmg(590)
+m.newProc(5)
+#
+m = Magic("Dark Forest","DF")
+m.newDmg(315)
+m.newDmg(lambda : 5 * math.floor(OWNED['MOUNTS']/3))
+m.newTrigTag('dragon')
+m.newTrig('spellcast','MID')
+m.newDmg(350)
+m.newTrigTag('beast')
+m.newTrig('spellcast','TotH')
+m.newDmg(350)
+m.newProc(10)
+#
+m = Magic("Typhoon","typh")
+m.newDmg(375)
+m.newTrigTag('abyssal')
+m.newDmg(25)
+m.newTrigTag('abyssal')
+m.newTrig('spellcast','Electrify')
+m.newDmg(25)
+m.newTrigTag('terror')
+m.newDmg(30)
+m.newTrigTag('terror')
+m.newTrig('spellcast','Crushing Pressure')
+m.newDmg(30)
+m.newProc(20)
+#
+m = Magic("Djinnpocalypse","Dj")
+#   This spell wants to know if you own Wish Warriors or Arch Djinn's Lamp.
+#   Not going to bother with that at this time, even if the damage could add up
+m.newDmg(1000)
+m.newTrigTag('magicalcreature')
+m.newDmg(150)
+m.newProc(10)
+#
+m = Magic("Eyes in the Dark","EitD")
+m.newDmg(300)
+m.newTrigTag('shadowelf')
+m.newDmg(66)
+m.newTrigTag('shadowelf')
+m.newTrig('spellcast','A Light in the Darkness')
+m.newDmg(26)
+m.newProc(30)
+#
+m = Magic("Blinding Light","BL")
+m.newDmg(100)
+m.newTrigTag('abyssal')
+m.newDmg(500)
+m.newTrigTag('terror')
+m.newDmg(100)
+m.newProc(10)
+m.newTrigTag('aquatic')
+m.newDmg(300)
+m.newProc(12)
+#
+m = Magic("Ice Storm","IS")
+m.newDmg(500)
+m.newTrigTag('winter')
+m.newDmg(90)
+m.newTrigTag('winter')
+m.newTrig('spellowned','Ice Storm')
+m.newDmg(60)
+m.newProc(15)
+#
+m = Magic("Gravebane","GB")
+m.newDmg(100)
+m.newTrigTag('undead')
+m.newDmg(500)
+m.newTrigTag('undead')
+m.newTrig('spellowned','Gravebane')
+m.newDmg(300)
+m.newProc(10)
+#
+m = Magic("Titan Killer","TK")
+m.newDmg(200)
+m.newTrigTag('giant')
+m.newDmg(1100)
+m.newDmg(lambda : 50 * OWNED['GIANTESSENCE'])
+m.newProc(6)
+#
+m = Magic("Bestial Downfall","BD")
+m.newDmg(30)
+m.newDmg(lambda : 4 * OWNED['BEASTMANESSENCE'])
+m.newTrigTag('beastman')
+m.newDmg(35)
+m.newProc(80)
+#
+m = Magic("Savage Melodies","SaM")  ## SM changed to SaM to avoid collision with Shattered Moon
+m.newTrigTag('giant')
+m.newDmg(500)
+m.newProc(20)
+#
+m = Magic("Intermission","int")
+m.newDmg(100)
+m.newDmg(lambda : 12 * OWNED['FESTIVALESSENCE'])
+m.newTrigTag('festival')
+m.newDmg(100)
+m.newProc(35)
+#
+m = Magic("Flame Serpent","FS")
+m.newDmg(250)
+m.newTrig('spellowned',"Flame Serpent")
+m.newDmg(200)
+m.newProc(15)
+#
+m = Magic("Annihilate","anni")
+m.newDmg(125)
+m.newTrigTag('construct')
+m.newDmg(300)
+m.newTrigTag('construct')
+m.newTrig('spellowned','Annihilate')
+m.newDmg(200)
+m.newProc(13)
+m.newDmg(600)
+m.newProc(2)
+#
+m = Magic("Conflagration","conf")
+#   Checks for Incinerated Soldiers owned. We aren't doing that here.
+m.newTrigTag('winter')
+m.newDmg(100)
+m.newTrigTag('winter')
+m.newTrig('spellowned','Living Flame')
+m.newDmg(100)
+m.newTrigTag('winter')
+m.newTrig('spellcast','Flame Serpent')
+m.newDmg(150)
+m.newProc(25)
+#
+m = Magic("What's At Stake","WaS")
+m.newDmg(100)
+m.newTrigTag('undead')
+m.newDmg(250)
+m.newTrigTag('dragon')
+m.newDmg(250)
+m.newProc(20)
+#
+m = Magic("Shattered Moon","SM")
+#   This checks if Ying of the Shattered Moon is owned. We're not doing that.
+m.newDmg(200)
+m.newTrig('spellowned','Shattered Moon')
+m.newDmg(100)
+m.newTrigTag('human')
+m.newDmg(750)
+m.newProc(10)
+#
+m = Magic("Guzzlebeard's Special Reserve","GSR")
+m.newDmg(100)
+m.newDmg(lambda : 5 * (OWNED['DWARFTROOPS'] + OWNED['DWARFGENERALS']) )
+m.newTrigTag('underground')
+m.newDmg(400)
+m.newTrig('spellcast','A Light in the Darkness')
+m.newDmg(300)
+m.newProc(10)
+#
+m = Magic("Acid Flask","AF")
+m.newDmg(100)
+m.newTrig('spellowned','Acid Flask')
+m.newDmg(150)
+m.newTrigTag('elite')
+m.newDmg(500)
+m.newProc(10)
+#
+m = Magic("Disintegrate","disi")
+m.newDmg(300)
+m.newTrig('spellowned','High Seas Enigma')
+m.newDmg(200)
+m.newTrig('spellowned','Volcanic Enigma')
+m.newDmg(200)
+m.newTrig('spellowned','Celestial Enigma')
+m.newDmg(200)
+m.newTrigTag('festival')
+m.newDmg(400)
+m.newProc(9)
+#
+m = Magic("High Seas Enigma","HSE")
+m.newDmg(150)
+m.newTrig('spellowned',"Volcanic Enigma")
+m.newDmg(100)
+m.newTrigTag('aquatic')
+m.newDmg(400)
+m.newTrigTag('aquatic')
+m.newTrig('spellcast',"Celestial Enigma")
+m.newDmg(200)
+m.newProc(11)
+#
+m = Magic("Celestial Enigma","CE")
+m.newDmg(150)
+m.newTrig('spellowned',"High Seas Enigma")
+m.newDmg(100)
+m.newTrigTag('deadly')
+m.newDmg(400)
+m.newTrigTag('deadly')
+m.newTrig('spellcast',"Volcanic Enigma")
+m.newDmg(200)
+m.newProc(11)
+#
+m = Magic("Volcanic Enigma","VE")
+m.newDmg(150)
+m.newTrig('spellowned',"Celestial Enigma")
+m.newDmg(100)
+m.newTrigTag('dragon')
+m.newDmg(400)
+m.newTrigTag('dragon')
+m.newTrig('spellcast',"High Seas Enigma")
+m.newDmg(200)
+m.newProc(11)
+#
+m = Magic("Wolpertinger Venom","WV")
+m.newDmg(15)
+m.newTrig('spellowned',"Death Echo")
+m.newDmg(70)
+m.newTrig('spellowned',"Deadly Strike")
+m.newDmg(70)
+m.newTrig('spellowned',"Wolpertinger Venom")
+m.newDmg(100)
+m.newTrig('spellowned',"Lesser Poison")
+m.newDmg(30)
+m.newTrig('spellowned',"Poison")
+m.newDmg(30)
+m.newTrig('spellowned',"Greater Poison")
+m.newDmg(30)
+m.newTrig('spellowned',"Assassin's Delight")
+m.newDmg(30)
+m.newTrig('spellowned',"Manticore Venom")
+m.newDmg(30)
+m.newProc(20)
+#
+m = Magic("Deadly Strike","DS")
+m.newDmg(800)
+m.newTrig('spellcast',"Death Echo")
+m.newDmg(400)
+m.newTrig('spellowned',"Deadly Strike")
+m.newDmg(100)
+m.newProc(5)
+#
+m = Magic("Death Echo","DE")
+m.newDmg(400)
+m.newTrig('spellowned',"Death Echo")
+m.newDmg(50)
+m.newProc(10)
+#
+m = Magic("Gag Gift","GG")
+m.newDmg(700)
+m.newTrig('spellowned',"Gag Gift")
+m.newDmg(100)
+m.newProc(5)
+#
+m = Magic("A Light in the Darkness","LitD")
+m.newDmg(40)
+m.newDmg( lambda : 15 * OWNED['UNDERGROUNDESSENCE'] )
+m.newTrigTag('underground')
+m.newDmg(600)
+m.newProc(7)
+#
+m = Magic("Guster's Fault","GF")
+#   This one is complicated. Did some preprocessing to make it fit our paradigm
+m.newDmg(190)
+m.newProc(20)
+#
+m = Magic("Heaven's Kiss","HvK")
+m.newDmg(10)
+m.newTrig('spellcast',"A Light in the Darkness")
+m.newDmg(50)
+m.newTrigTag('shadowelf')
+m.newDmg(700)
+m.newTrigTag('elite')
+m.newDmg(600)
+m.newProc(12)
+#
+m = Magic("Lesser Poison","LP")
+m.newDmg(2)
+m.newProc(4)
+#
+m = Magic("Poison","P")
+m.newDmg(4)
+m.newProc(7)
+#
+m = Magic("Greater Poison","GP")
+m.newDmg(8)
+m.newProc(15)
+#
+m = Magic("Assassin's Delight","AD")
+m.newDmg(50)
+m.newTrig('spellcast',"Lesser Poison")
+m.newDmg(50)
+m.newTrig('spellcast',"Poison")
+m.newDmg(50)
+m.newTrig('spellcast',"Greater Poison")
+m.newDmg(50)
+m.newProc(8)
+#
+m = Magic("Manticore Venom","MV")
+m.newDmg(100)
+m.newTrig('spellowned',"Manticore Venom")
+m.newDmg(100)
+m.newTrig('spellowned',"Fey Flame")
+m.newDmg(100)
+m.newTrig('spellowned',"Dragon's Breath")
+m.newDmg(100)
+m.newTrig('spellowned',"Poison")
+m.newDmg(25)
+m.newTrig('spellowned',"Greate Poison")
+m.newDmg(25)
+m.newTrig('spellowned',"Lesser Poison")
+m.newDmg(25)
+m.newTrig('spellowned',"Assassin's Delight")
+m.newDmg(25)
+m.newTrigTag('beast')
+m.newDmg(250)
+m.newProc(5)
+#
+m = Magic("Soul Pilferer","SP")
+#   Checks for number of Grave Guardians owned. We don't do this.
+m.newTrigTag('undead')
+m.newDmg(300)
+m.newTrigTag('undead')
+m.newTrig('spellcast',"Gravebane")
+m.newDmg(400)
+m.newProc(15)
+#
+m = Magic("Blessing of Mathala","BoM")
+#   This magic checks for those gauntlet trophies. Not doing that here.
+m.setrare()
+m.newDmg(850)
+m.newTrig('spellowned',"BoM")
+m.newDmg(100)
+m.newProc(12)
+#
+m = Magic("SMITE","SMITE")
+#   This magic also checks for those gauntlet trophies. Not doing that here.
+m.setrare()
+m.newDmg(500)
+m.newTrig('spellowned',"SMITE")
+m.newDmg(150)
+m.newProc(22)
+#
+m = Magic("Veil Dust","VD")
+m.newDmg(300)
+m.newTrig('spellowned',"Veil Dust")
+m.newDmg(50)
+m.newTrig('spellowned',"Fey Flame")
+m.newDmg(50)
+m.newTrigTag('magicalcreature')
+m.newDmg(400)
+m.newProc(10)
+#
+m = Magic("Fey Flame","FF")
+#   This checks for Kindly Folk set. We're not going to do that.
+m.newDmg(25)
+m.newDmg( lambda : OWNED['MAGICALBEINGTROOPS'] + OWNED['MAGICALBEINGGENERALS'] )
+m.newTrig('spellowned',"Fey Flame")
+m.newDmg(25)
+m.newTrig('spellowned',"Dragon's Breath")
+m.newDmg(25)
+m.newTrig('spellowned',"Manticore Venom")
+m.newDmg(25)
+m.newTrigTag('human')
+m.newDmg(200)
+m.newProc(10)
+#
+m = Magic("Dragon's Breath","DB")
+m.newDmg(25)
+m.newDmg( lambda : 5 * (OWNED['DRAGONESSENCE'] + OWNED['DRAGONMOUNTS']) )
+m.newTrig('spellowned',"Dragon's Breath")
+m.newDmg(25)
+m.newTrig('spellowned',"Fey Flame")
+m.newDmg(25)
+m.newTrig('spellowned',"Manticore Venom")
+m.newDmg(25)
+m.newProc(10)
+#
+m = Magic("Visions of the Deep","VotD")
+#   This counts Depth Terror set. We're not doing that.
+m.newDmg(500)
+m.newTrigTag(['terror','abyssal']) #OR condition. Apply proc once.
+m.newDmg(58)
+m.newTrigTag('terror')
+m.newTrig('spellcast',"Typhoon")
+m.newDmg(28)
+m.newProc(20)
+#
+m = Magic("Putrid Swamp","PS")
+m.newDmg(825)
+m.newTrigTag(['orc','ogre','goblin']) #OR CONDITION
+m.newTrig('spellcast',"Pestilent Bolt")
+m.newDmg(125)
+m.newTrigTag('dragon')
+m.newTrig('spellcast',"Contagion")
+m.newDmg(125)
+m.newTrigTag(['human','beastman']) #OR CONDITION
+m.newTrig('spellcast',"Noxious Breath")
+m.newDmg(125)
+m.newProc(10)
+#
+m = Magic("Pestilent Bolt","PB")
+m.newDmg(550)
+m.newTrigTag(['orc','ogre','goblin']) #OR CONDITION
+m.newTrig('spellcast',"Pestilent Bolt")
+m.newDmg(170)
+m.newProc(15)
+#
+m = Magic("Contagion","cont")
+m.newDmg(1650)
+m.newTrigTag('dragon')
+m.newDmg(510)
+m.newProc(5)
+#
+m = Magic("Noxious Breath","NB")
+m.newDmg(400)
+m.newTrigTag(['human','beastman']) #OR CONDITION
+m.newDmg(140)
+m.newProc(20)
+
+
+
+
+
+
+'''
+m = Magic("","")
+m.newDmg()
+m.newTrigTag('')
+m.newDmg()
+m.newProc()
+'''
+
+        
+#RAIDTAGS.append('dragon')  #DEBUG
+print "Raid magic, calc for tags: "+str(RAIDTAGS)
+
+
+print "-------------"
+# Get noncast averages of each spell into ALLMAGICS list.
+ALLMAGICS = []
+for i in Magic.spelllist:
+    avg = i.getAvg()
+    ALLMAGICS.append([i,avg])
+    if SHOWDEBUG: print "Magic: "+str(i.nickname)+", avg dmg: "+str(avg)
+#Sort ALLMAGICS and skim off top into MAGICLIST to start calc for NEWMAGICS
+ALLMAGICS = sorted(ALLMAGICS,key = lambda x: x[1],reverse = True)
+MAGICLIST = copy.deepcopy(ALLMAGICS[:SLOTNUM+5])
+NEWMAGICS = []
+for i,oavg in MAGICLIST:
+    avg = i.getAvg()
+    NEWMAGICS.append([i,avg])
+NEWMAGICS = sorted(NEWMAGICS,key = lambda x: x[1],reverse = True)
+#And now we should have a roughly optimized table.
+
+shortlist = ""
+for idx,i in enumerate(range(SLOTNUM+5)):
+    spell = NEWMAGICS[i][0]
+    avg   = NEWMAGICS[i][1]
+    if idx==SLOTNUM: print "-------------"
+    print str(spell.fullname)+": "+str(avg)
+    shortlist += spell.nickname+","
+print "-------------"
+print "Short list:"
+shortlist = shortlist[:-1]
+print shortlist
+
---
+++ readme.md
@@ -0,0 +1,81 @@
+DotD Magic Calculator
+=====================
+
+Requirement(s)
+--------------
+* Python 2.7.x
+
+What is this?
+-------------
+Magic. It's a great thing. Stick it on raids and watch the damage go up. But
+people get real picky about which to apply where, and others have problems
+keeping up with what the best should be.
+
+This utility is designed to find the best (and next-to-best) magic for any raid
+given its tags, which are supplied on the command-line, and the user's status
+and parameters (e.g. number of mounts owned) in the script itself.
+
+How to use
+----------
+Assuming you've installed Python 2.7.x (3.x will probably fail horribly), open
+up a command prompt wherever the script is and type `python dotdmagi.py <N> [T1] ... [TN]`
+where `N` is the number of magic slots the raid has, and `T1` through `TN` are
+the tags (e.g. orc, dragon, deadly, etc.) that the raid has.
+
+Example 1: You want to find the optimal magic setup for the raid Deadly Drakontos.
+You look up the raid and find that it has eight magic slots and
+the tags `Deadly , Aquatic , Dragon , Terror`. You would type the following into
+the command-line:
+
+`python dotdmagi.py 8 deadly aquatic dragon terror`
+
+Example 2: You want to do the same with Deadly Vas'ok. It too has eight magic
+slots, but has a load more tags: `Deadly , Dragon , Shadow Elf , Magical Creature , Underground`
+
+`python dotdmagi.py 8 deadly dragon shadowelf magicalcreature underground`
+
+Example 3: You want to beat up poor Horgrak. Not even the deadly version, just
+the poor, abused, oft-slaughtered kobold. He has two magic slots and no tags.
+
+`python dotdmagi.py 2`
+
+Example 4: Someone gave you a link to a Deadly Black Hand Lieutenant, and it's
+your job to help magick it up. It has five magic slots and the tags `Black Hand , Winter , Human , Deadly`
+
+`python dotdmagi.py 5 blackhand winter human deadly`
+
+Modifying What The Script Assumes You Own
+-----------------------------------------
+If you open up `dotdmagi.py` in a text/code editor (**NOT** a word processor. Those
+will mangle the source), look a few lines down for the line that starts with
+`OWNED`. That signals the start of the dictionary showing what the script assumes
+you own. Most significant are the mounts you own, number of legions, and what
+spells you own.
+
+Most entries are straightforward; they require no explanation. `SPELLS`, on the
+other hand, is a list which can contain either spell abbreviation/nicknames or
+their full name. You should probably use the full name, as they are exactly as
+they appear in-game, including capitalization and punctuation.
+
+
+Known Issues
+------------
+
+* All tags had their spaces stripped to form a single word for each. This is a 
+  limitation of the command-line interface without needing to use a bunch of quotes
+* All tags are also lowercase. Searches are case-sensitive and if you include any
+  capital letters in the names, they're skipped.
+* If you edit ANYTHING inside the script, all names are case-sensitive too.
+  Including spell names and abbreviations/nicknames.
+* There are quite a few magics whose calculations have been simplified, either
+  via manual calculation or by removing their effect altogether (as it's not
+  expected many, if any, will meet those requirements). Those simplifications
+  are noted with the magic definition.
+
+License
+-------
+It's the MIT License. See `LICENSE` for more details.
+
+Version History
+---------------
+* 0.1 - First release. Seems to largely work
